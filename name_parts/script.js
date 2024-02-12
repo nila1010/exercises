8bf33ef869165de5f7f839f9edf2fb70e507315e
NL <nila0001@stud.kea.dk>
2024-02-12 09:41:09 +0100

    split name part

--- name_parts/script.js
+++ name_parts/script.js
@@ -5,3 +5,10 @@ const middleName = person.substring(person.indexOf(" ") + 1, person.lastIndexOf(
 const lastName = person.substring(person.lastIndexOf(" ") + 1);
 
 console.log(firstName, middleName, lastName);
+
+const personSplit = person.split(" ");
+const personSplitFirstName = personSplit[0];
+const personSplitMiddleName = personSplit[1];
+const personSplitLastName = personSplit[2];
+
+console.log(personSplitFirstName, personSplitMiddleName, personSplitLastName);
